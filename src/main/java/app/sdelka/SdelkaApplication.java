3f79d5a4d15eb764dcf53dae7c420025282d57b7
Teemitze <teemitze@gmail.com>
2020-11-08 13:12:30 +0300

    Initial commit

---
+++ src/main/java/app/sdelka/SdelkaApplication.java
@@ -0,0 +1,13 @@
+package app.sdelka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdelkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdelkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/sdelka/SdelkaApplicationTests.java
@@ -0,0 +1,13 @@
+package app.sdelka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdelkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
