a031f852c30b8f6c429b2f11e7875e06f3db72c5
Teemitze <teemitze@gmail.com>
2020-11-15 10:30:38 +0300

    Create schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS adverts;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id    SERIAL PRIMARY KEY,
+    name  VARCHAR NOT NULL,
+    phone VARCHAR NOT NULL,
+    city VARCHAR NOT NULL,
+    email VARCHAR NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS categories
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL UNIQUE
+);
+
+
+CREATE TABLE IF NOT EXISTS adverts
+(
+    uuid       UUID PRIMARY KEY,
+    name     VARCHAR NOT NULL,
+    description TEXT,
+    price    BIGINT DEFAULT 0,
+    city     VARCHAR,
+    create_data DATE NOT NULL,
+    is_active boolean NOT NULL,
+    is_new boolean NOT NULL,
+    photo_path VARCHAR[],
+    category_id INTEGER REFERENCES categories (id),
+    user_id  INTEGER REFERENCES users (id)
+);
